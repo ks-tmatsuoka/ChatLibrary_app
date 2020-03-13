7e529c19658bb014568e2d5fc76f611ce1e6c7b5
Iga <iga@entap.co.jp>
2020-03-13 17:11:19 +0900

    [update]AssemblyInfo : iOS, AndroidプロジェクトからのInternalsVisibleTo設定

--- Entap.Chat/Entap.Chat/AssemblyInfo.cs
+++ Entap.Chat/Entap.Chat/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: InternalsVisibleTo("Entap.Chat.iOS")]
+[assembly: InternalsVisibleTo("Entap.Chat.Android")]
