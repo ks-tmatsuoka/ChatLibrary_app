438c6976586f0b37a29d07b9003ae81864065ce1
Iga <iga@entap.co.jp>
2020-03-18 12:36:32 +0900

    [add]フォルダ、ファイル追加
    Models/BindableBase,
    Pages/PageViewModelBase

---
+++ Entap.Chat/Samples/ChatSample/Models/BindableBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace ChatSample
+{
+    /// <summary>
+    /// Bindableオブジェクトの基底クラス
+    /// </summary>
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        /// <summary>
+        /// プロパティ値に変更がある場合に、クラアントに通知する。
+        /// </summary>
+        /// <typeparam name="T">プロパティの型。</typeparam>
+        /// <param name="storage">get アクセス操作子と set アクセス操作子両方を使用したプロパティへの参照。</param>
+        /// <param name="value">プロパティに必要な値。</param>
+        /// <param name="propertyName">リスナーに通知するために使用するプロパティの名前。省略可能。</param>
+        /// <returns><c>true</c>プロパティに変更あり</returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Entap.Chat/Samples/ChatSample/Pages/PageViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System;   
+namespace ChatSample
+{
+    public class PageViewModelBase : BindableBase
+    {
+        public PageViewModelBase()
+        {
+        }
+    }
+}
