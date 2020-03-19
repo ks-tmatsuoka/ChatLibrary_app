2dc64dfd8833ccc68a00016096a7cdbdbaeabb1e
Iga <iga@entap.co.jp>
2020-03-19 16:53:49 +0900

    [add]Entap.Chat : ChatListViewRenderer追加（iOS, Android）

---
+++ Entap.Chat/Entap.Chat.Android/Renderers/ChatListViewRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Android.Content;
+using Android.Runtime;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entap.Chat.ChatListView), typeof(Entap.Chat.Android.ChatListViewRenderer))]
+namespace Entap.Chat.Android
+{
+    [Preserve(AllMembers = true)]
+    public class ChatListViewRenderer : CollectionViewRenderer
+    {
+        public ChatListViewRenderer(Context context) : base(context)
+        {
+        }
+    }
+}
---
+++ Entap.Chat/Entap.Chat.iOS/Renderers/ChatListViewRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entap.Chat.ChatListView), typeof(Entap.Chat.iOS.ChatListViewRenderer))]
+namespace Entap.Chat.iOS
+{
+    [Preserve(AllMembers = true)]
+    public class ChatListViewRenderer : CollectionViewRenderer
+    {
+    }
+}
