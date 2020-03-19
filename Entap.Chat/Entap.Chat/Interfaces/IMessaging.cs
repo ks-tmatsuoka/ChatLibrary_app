9dc41c647fedb8748846640853a9850b8691e7d8
Iga <iga@entap.co.jp>
2020-03-19 16:39:21 +0900

    [add]Entap.Chat : メッセージ制御のInterface（IMessaging）追加

---
+++ Entap.Chat/Entap.Chat/Interfaces/IMessaging.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Entap.Chat
+{
+    public interface IMessaging
+    {
+        Task<IEnumerable<MessageBase>> GetMessagesAsync(int index, int count);
+        Task<IEnumerable<MessageBase>> GetNewMessagesAsync(int index, int count);
+    }
+}
