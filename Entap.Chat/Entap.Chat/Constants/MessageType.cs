ed394f36d87e652109be997d48eda8193bc3fc18
Iga <iga@entap.co.jp>
2020-03-19 16:33:27 +0900

    [add]Entap.Chat : Constants/MessageType

---
+++ Entap.Chat/Entap.Chat/Constants/MessageType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Entap.Chat
+{
+    /// <summary>
+    /// メッセージタイプ
+    /// </summary>
+    public enum MessageType
+    {
+        None,
+        Text,
+        Image,
+    }
+}
