9347aaaf15a8deff91541adda7756d65f2af8a5a
seki <seki@entap.co.jp>
2020-03-24 12:22:43 +0900

    [add] BottomControllerファイル追加

---
+++ Entap.Chat/Entap.Chat/Controls/BottomController.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Entap.Chat.Controls
+{
+    public partial class BottomController : ContentView
+    {
+        public BottomController()
+        {
+            InitializeComponent();
+        }
+    }
+}
