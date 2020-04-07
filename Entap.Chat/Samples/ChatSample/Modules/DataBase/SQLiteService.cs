824f8636e21c0a4950fcd01f95ea5e107a902d8a
seki <seki@entap.co.jp>
2020-04-07 16:48:51 +0900

    [update] SQLiteService 変更

--- Entap.Chat/Samples/ChatSample/Modules/DataBase/SQLiteService.cs
+++ Entap.Chat/Samples/ChatSample/Modules/DataBase/SQLiteService.cs
@@ -12,21 +12,34 @@ namespace ChatSample
                 "EntapChat.db3");
 
         static SQLiteAsyncConnection _asyncConnection;
-        public static SQLiteAsyncConnection AsyncConnection = _asyncConnection ?? (_asyncConnection = GetAsyncConnection());
+        //public static SQLiteAsyncConnection AsyncConnection = _asyncConnection ?? (_asyncConnection = GetAsyncConnection());
+        //public static SQLiteAsyncConnection GetAsyncConnection()
+        //{
+        //    return new SQLiteAsyncConnection(DatabasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.Create);
+        //}
 
-        public static SQLiteAsyncConnection GetAsyncConnection()
+        public static SQLiteAsyncConnection AsyncConnection => AsyncConnectionLazyInitializer.Value;
+        static Lazy<SQLiteAsyncConnection> AsyncConnectionLazyInitializer = new Lazy<SQLiteAsyncConnection>(() =>
         {
             return new SQLiteAsyncConnection(DatabasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.Create);
-        }
+        });
+
 
         static SQLiteConnection _connection;
-        public static SQLiteConnection Connection = _connection ?? (_connection = GetConnection());
+        //public static SQLiteConnection Connection = _connection ?? (_connection = GetConnection());
+        //public static SQLiteConnection GetConnection()
+        //{
+        //    var conn = new SQLiteConnection(DatabasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.Create);
+        //    conn.BusyTimeout = TimeSpan.FromSeconds(1);
+        //    return conn;
+        //}
 
-        public static SQLiteConnection GetConnection()
+        public static SQLiteConnection Connection => ConnectionLazyInitializer.Value;
+        public static Lazy<SQLiteConnection> ConnectionLazyInitializer = new Lazy<SQLiteConnection>(() =>
         {
             var conn = new SQLiteConnection(DatabasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.FullMutex | SQLiteOpenFlags.Create);
             conn.BusyTimeout = TimeSpan.FromSeconds(1);
             return conn;
-        }
+        });
     }
 }
